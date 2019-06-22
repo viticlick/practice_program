af2d90295fe517f63a236094fa8d8b788c52c2c6
viti <viti@192.168.0.22>
2019-06-22 16:37:03 +0200

    Add failing tests

--- katas_java/05-refactoring-awful-inheritance-use-with-beverage-prices-kata/src/test/java/unit_tests/BeveragesPricingTest.java
+++ katas_java/05-refactoring-awful-inheritance-use-with-beverage-prices-kata/src/test/java/unit_tests/BeveragesPricingTest.java
@@ -13,6 +13,12 @@ public class BeveragesPricingTest {
         Beverage coffee = new Coffee();
         assertThat(coffee.price(), is(closeTo(1.20, 0.001)));
     }
+    
+    @Test
+    public void computes_coffee_and_cinnamon_price() {
+        Beverage beverage = new Cinnamon(new Coffee());
+        assertThat(beverage.price(), is(closeTo(1.25, 0.001)));
+    }
 
     @Test
     public void computes_tea_price() {
@@ -21,15 +27,33 @@ public class BeveragesPricingTest {
     }
 
     @Test
+    public void computes_tea_and_cinnamon_price() {
+        Beverage beverage = new Cinnamon(new Tea());
+        assertThat(beverage.price(), is(closeTo(1.55, 0.001)));
+    }
+
+    @Test
     public void computes_hot_chocolate_price() {
         Beverage hotChocolate = new HotChocolate();
         assertThat(hotChocolate.price(), is(closeTo(1.45, 0.001)));
+    }
+    
+    @Test
+    public void computes_hot_chocolate_and_cinnamon_price() {
+        Beverage beverage = new Cinnamon(new HotChocolate());
+        assertThat(beverage.price(), is(closeTo(1.50, 0.001)));
     }
 
     @Test
     public void computes_tea_with_milk_price() {
         Tea teaWithMilk = new TeaWithMilk();
         assertThat(teaWithMilk.price(), is(closeTo(1.60, 0.001)));
+    }
+    
+    @Test
+    public void computes_tea_with_milk_and_cinnamon_price() {
+        Beverage beverage = new Cinnamon(new TeaWithMilk());
+        assertThat(beverage.price(), is(closeTo(1.65, 0.001)));
     }
 
     @Test
@@ -39,14 +63,32 @@ public class BeveragesPricingTest {
     }
 
     @Test
+    public void computes_coffee_with_milk_and_cinnamon_price() {
+        Beverage beverage = new Cinnamon(new CoffeeWithMilk());
+        assertThat(beverage.price(), is(closeTo(1.35, 0.001)));
+    }
+    
+    @Test
     public void computes_coffee_with_milk_and_cream_price() {
         Coffee coffeeWithMilkAndCream = new CoffeeWithMilkAndCream();
         assertThat(coffeeWithMilkAndCream.price(), is(closeTo(1.45, 0.001)));
+    }
+    
+    @Test
+    public void computes_coffee_with_milk_and_cream_and_cinnamon_price() {
+        Beverage beverage= new Cinnamon(new CoffeeWithMilkAndCream());
+        assertThat(beverage.price(), is(closeTo(1.50, 0.001)));
     }
 
     @Test
     public void computes_hot_chocolate_with_cream_price() {
         HotChocolateWithCream hotChocolateWithCream = new HotChocolateWithCream();
         assertThat(hotChocolateWithCream.price(),  is(closeTo(1.60, 0.001)));
+    }
+
+    @Test
+    public void computes_hot_chocolate_with_cream_and_cinnamon_price() {
+    	Beverage beverage = new Cinnamon(new HotChocolateWithCream());
+    	assertThat(beverage.price(), is(closeTo(1.65, 0.001)));
     }
 }
